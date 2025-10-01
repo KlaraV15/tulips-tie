e7915b3d3cf6242edb3cea1ea66ca05cc4ff8b82
Damian <damianvandernat@gmail.com>
2025-10-01 08:30:46 +0200

    made test mail functionality

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/send-mail', [App\Http\Controllers\MailController::class, 'sendMail']);
+
+Route::get('/test-reset/{email}', function ($email) {
+    $user = \App\Models\User::where('email', $email)->firstOrFail();
+    return app()->call('App\Http\Controllers\MailController@sendResetMail', ['user' => $user]);
+});
+
